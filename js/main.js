df7f3a0e85651b9e357d582188577c2ab5d6732a
Martin Muñoz <mmvdp89@gmail.com>
2022-11-08 17:38:01 -0300

    Agregados de imagenes y modificaciones en JS

--- js/main.js
+++ js/main.js
@@ -1,77 +1,156 @@
 
 
-// const mayorEdad = () => prompt("Hola, " + nombre + ". " + "Indicanos tu edad")
+
 let edadUsuario = 0
 let eleccion
 let cantidadCompra
-let newUser
-let newPass
-let usuario
-let pass
-let stockPioneroAzul = 5
+let nombre = ""
+var newUser
+var newPass
+var usuario
+var pass
 
-let nombre = prompt("Bienvenido.\n" + "Diganos su nombre por favor")
 
-while (nombre === "") {
-    Alert = ("Usted no ha ingresado datos")
-    nombre = prompt("Bienvenido.\n" + "Diganos su nombre por favor");
-}
+//----------------------------------------------------- Comienzo de arrays
 
-do {
-    edadUsuario = Number(prompt("Hola, " + nombre + ". " + "Indicanos tu edad"))
-}
-while (edadUsuario < 18);
+const articulos = [
+    {
+        Id: "1",
+        Industria: "Nacional",
+        articulo: "Boxer",
+        Marca: ["Pionero Real", "UOMO", "Lody"],
+        modelosPionero: [
+            { Color: "Azul", Precio: 2200, Stock: 10 },
+            { Color: "Rojo", Precio: 1400, Stock: 2 },
+            { Color: "Negro", Precio: 1000, Stock: 10 },
+            { Color: "Celeste", Precio: 3000, Stock: 8 }],
+        modelosUomo: [
+            { Modelo: "Simpsons", Precio: 1500, Stock: 10 },
+            { Color: "Batman", Precio: 1500, Stock: 10 },
+            { Color: "Chicago", Precio: 1500, Stock: 10 },
+            { Color: "Robot", Precio: 1500, Stock: 10 }],
+        modelosLody: [
+            { Color: "Verde", Precio: 1500, Stock: 10 },
+            { Color: "Rojo", Precio: 1500, Stock: 10 },
+            { Color: "Azul", Precio: 1500, Stock: 10 }],
 
-if (edadUsuario > 18) {
-    alert("Bienvenido al mundo del boxer\n" + "Es necesario crear un usuario para realizar una compra:");
-    userNuevo();
-    corroborarUser();
+    },
+    {
+        Id: "2",
+        Industria: "Importado",
+        Marca: ["barak", "Men"],
+        articulo: "Boxer",
+        modelosBarak: ["Fucsia", "Turquesa", "Gris"],
+        modelosMen: ["Negro", "Azul", "Gris"],
+        Precio: 1500,
+        Stock: 10,
+    },
+    {
+        Id: "3",
+        Industria: "Nacional",
+        Marca: ["Dufour", "Rhyton"],
+        articulo: "Medias",
+        modelosDufour: ["Negro,Blanco,Gris"],
+        modelosRhyton: ["Negro,Azul,Blanco"],
+        Precio: 1000,
+        Stock: 10,
+    },
 
-} else {
-    alert("Disculpa, eres menor, no puedes ingresar. Muchas gracias.");
+]
 
-}
 
-//Comienzo de funciones
 
-function userNuevo() {
+//----------------------------------------------------Comienzo de funciones
+function pedirNombre() {
+
     do {
-        let newUser = prompt("Ingresa tu nuevo nombre de usuario por favor:");
+        nombre = prompt("Bienvenido.\n" + "Diganos su nombre por favor");
 
-        let newPass = prompt("Ahora ingresa tu contraseña:");
+        if (textoNullOVacio(nombre)) {
+            alert("Usted no ha ingresado datos");
+        }
+        //el while es como el if anterior pero genero que se vuelva a crear la pregunta    
+    } while (textoNullOVacio(nombre));
+}
 
-        if ((newUser == null || newPass == null) && (newUser == "") || (newPass == "")) {
-            alert("Datos invalidos. Intente nuevamente")
+function pedirYValidarEdad() {
+    let esMayorOIgualA18 = false
+    do {
+        //pido la edad y la recibo en texto.
+        let edadIngresadaEnTexto = prompt("Hola, " + nombre + ". " + "Indicanos tu edad")
+
+        //me fijo si es un numero, llamando a la funcion esNumerico (devuelve true o false).
+        let esNumeroONo = esNumerico(edadIngresadaEnTexto);
+
+        //si es un numero, lo convierto a Number.
+        if (esNumeroONo) {
+            edadUsuario = Number(edadIngresadaEnTexto);
+
+            esMayorOIgualA18 = (edadUsuario >= 18);
+
+            if (esMayorOIgualA18 == false) {
+                alert("No puedes ingresar, eres menor.");
+            }
+        } else {
+            alert("Usted no ha ingresado un numero");
         }
 
-    } while (newUser == null || newPass == null);
-    alert("Usuario Creado. A continuación deberás reingresar con tu Usuario y Clave")
+        //Me fijo si es mayor de edad, y lo guardo en una variable booleana.
+
+
+    } while (esMayorOIgualA18 == false);
 }
 
-function corroborarUser() {
-
-    let usuario = prompt("Reingrese su usuario");
-
-    let pass = prompt("Reingrese su password");
-
-    if ((newUser === usuario) && (newPass === pass)) {
-        alert("Sus datos fueron ingresados correctamente.");
-        seleccionProducto();
+//Comprueba si es un numero o no, devuelve true o false.
+function esNumerico(textoAEvaluar) {
+    if (/^\d+$/.test(textoAEvaluar)) {
+        return true;
     } else {
-        alert("Sus datos ingresados no son validos");
-        corroborarUser();
+        return false;
     }
 }
 
+function textoNullOVacio(texto) {
+    return texto === null || texto === "";
+
+}
+
+function userNuevo() {
+    do {
+        newUser = prompt("Ingresa tu nuevo nombre de usuario por favor:")
+        newPass = prompt("Ahora ingresa tu contraseña:")
+
+        if (textoNullOVacio(newUser) || textoNullOVacio(newPass)) {
+            alert("Datos invalidos. Intente nuevamente")
+        }
+
+    } while (textoNullOVacio(newUser) || textoNullOVacio(newPass));
+
+    alert("Usuario Creado. A continuación deberás reingresar con tu Usuario y Clave");
+
+
+}
+
+function corroborarUser() {
+    do {
+        usuario = prompt("Reingrese su usuario");
+
+        pass = prompt("Reingrese su password");
+
+        if (newUser != usuario || newPass != pass) {
+            alert("Usuario y/o contraseña erroneos. Intente nuevamente.")
+        }
+    } while (newUser != usuario || newPass != pass)
+
+    alert("Sus datos fueron ingresados correctamente.");
+
+}
+
+
 function seleccionProducto() {
 
-    console.log("----------------------");
-    console.log("Que producto deseas comprar?");
-    console.log("1 - Boxer Nacionales");
-    console.log("2 - Boxer Importados");
-    console.log("3 - Medias");
+    let eleccion = prompt("----------------------\n" + "Que producto deseas comprar?\n" + "\n" + "1 - Boxer Nacionales\n" + "2 - Boxer Importados\n" + "3 - Medias\n" + "\n" + "Elija la opcion deseada:")
 
-    let eleccion = prompt("Elija la opcion deseada:");
     switch (eleccion) {
         case "1":
             boxerNacionales()
@@ -80,7 +159,7 @@ function seleccionProducto() {
             boxerImportados()
             break
         case "3":
-            Medias()
+            medias()
             break
         default:
             alert = ("Opcion incorrecta");
@@ -90,91 +169,101 @@ function seleccionProducto() {
 }
 
 function boxerNacionales() {
-    console.log("----------------------");
-    console.log("1 - Pionero Real - Azul - $1200");
-    console.log("2 - Pionero Real -Rojo $1200");
-    console.log("3 - Pionero Real - Negro $1200");
-    console.log("4 - Pionero Real - Celeste $1200");
-    console.log("5 - Volver al menu anterior");
 
-    let eleccion = prompt("Elija el color");
-    switch (eleccion) {
-        case "1":
-            compra()
-            break
-        case "2":
-            compra()
-            break
-        case "3":
-            compra()
-            break
-        case "4":
-            compra()
-            break
-        case "5":
-            seleccionProducto();
-            break
-        default:
-            alert = ("Opcion incorrecta");
+    do {
 
-            break
-    }
+        let eleccion = prompt("Que marca buscabas: Pionero Real, Uomo o Lody?");
+
+        if (eleccion == "Pionero Real" || eleccion == 1) {
+
+            alert("Haz elegido Pionero Real" + " y los modelos son: \n" + articulos[0].modelosPionero[0].Color + "\n" + articulos[0].modelosPionero[1].Color + "\n" + articulos[0].modelosPionero[2].Color + "\n" + articulos[0].modelosPionero[3].Color)
+            consultaPorPrecio();
+            // consultaPorModelo();
+
+        } else if (eleccion == "Uomo" || eleccion == 2) {
+            console.log((articulos[0].Marca[1]), (articulos[0].modelosUomo));
+
+        } else if (eleccion == "Lody" || eleccion == 3) {
+            console.log((articulos[0].Marca[2]), (articulos[0].modelosLody))
+
+        } else { alert(" Opcion incorrecta.") };
+
+    } while (articulos.filter(p => p.Marca === eleccion).length === 0 && eleccion != "1" && eleccion != "2" && eleccion != "3")
+
 }
+
 
 function boxerImportados() {
 
-    console.log("----------------------");
-    console.log("1 - Azul");
-    console.log("2 - Rojo");
-    console.log("3 - Negro");
-    console.log("4 - Celeste");
-    console.log("5 - Volver al menu anterior");
+    console.log((articulos[1].Marca[0]), (articulos[1].modelosBarak), (articulos[1].Precio));
+    console.log((articulos[1].Marca[1]), (articulos[1].modelosMen), (articulos[1].Precio));
 
-    let eleccion = prompt("Elija el color");
-    switch (eleccion) {
-        case "1":
-            compra()
-            break
-        case "2":
-            compra()
-            break
-        case "3":
-            compra()
-            break
-        case "4":
-            compra()
-            break
-        case "5":
-            seleccionProducto();
-            break
-        default:
-            alert("Opcion incorrecta");
-            boxerImportados();
-            break
-    }
-}
-
-function compra() {
-    let cantidadCompra = prompt("Cuantos desea comprar?");
-    switch (cantidadCompra) {
-        case "1":
-            if (cantidadCompra <= stockPioneroAzul) {
-                Confirm = ("Usted selecciono " + cantidadCompra + ". " + "Es Correcto?")
-            }
-            else if (confirm) {
-                Pagar();
-            }
-            else {
-                Alert = ("Tenemos" + stockPioneroAzul + "en stock. ");
-                compra();
-            }
-            break
-        case "2":
-            console.log = ("hola")
-            break
-        case "3":
-
-            break
-    }
 
 }
+
+function medias() {
+
+    console.log((articulos[2].modelosDufour), (articulos[2].Precio));
+    console.log((articulos[2].modelosRhyton), (articulos[2].Precio));
+
+
+}
+
+function consultaPorPrecio() {
+
+    // eleccion = prompt("Hasta que precio buscabas?");
+    articulos.filter((producto) => {
+
+        alert(producto.Id)
+
+    }
+    )
+
+}
+
+// function consultaPorModelo() {
+//     eleccion = prompt("Tenemos estos disponibles: " + (articulos[0].modelosPionero[0].Color) + " " + (articulos[0].modelosPionero[1].Color) + " " + (articulos[0].modelosPionero[2].Color) + " " + (articulos[0].modelosPionero[3].Color) + ". De que precio buscabas?")
+
+//     if (eleccion === ) {
+
+
+//         alert("Estos son los articulos menores a 150 unidades");
+//         console.log(modelosPionero)
+//     } else {
+//         alert("todo mal")
+//     }
+
+
+// }
+
+
+
+function Suma(primerNumero, segundoNumero) {
+    return primerNumero + segundoNumero;
+}
+
+let numeroUno = 5;
+let numeroDos = 3;
+
+let sumatoria = Suma(numeroUno, numeroDos);
+
+
+
+//--------------------------------------------------------INICIO DE SISTEMA
+
+pedirNombre();
+
+pedirYValidarEdad();
+
+alert("Bienvenido al mundo del boxer\n" + "Es necesario crear un usuario para realizar una compra:");
+
+userNuevo();
+
+corroborarUser();
+
+seleccionProducto();
+
+
+
+
+//--------------------------------------------------------FIN DEL SISTEMA
