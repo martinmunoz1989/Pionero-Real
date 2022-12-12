a7fced508764bfb5987db6ac03294673b04f1647
Martin Muñoz <mmvdp89@gmail.com>
2022-12-12 00:48:09 -0300

    modificaciones generales, storage

--- js/main1.js
+++ js/main1.js
@@ -21,10 +21,16 @@ function textoNullOVacio(texto) {
 
 }
 
+//////// STORAGE
+
+const saveLocal = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+
+//////// Inicio sistema
 
 function sistema(divs) {
 
-    //////// fotos
 
     articulos.forEach((articulo) => {
         let div = document.createElement("div");
@@ -63,8 +69,14 @@ function sistema(divs) {
                 descripcion: articulo.descripcion,
                 precio: articulo.Precio,
             });
-
-
+            Toastify({
+                text: "Has agregado un articulo",
+                className: "info",
+                style: {
+                    background: "linear-gradient(to right, #00b09b, #96c93d)",
+                }
+            }).showToast();
+            saveLocal()
 
         })
         div.appendChild(divDescripcion);
@@ -80,6 +92,8 @@ function sistema(divs) {
         //// orden Header
         orden.style.display = "block";
         orden.replaceChildren();
+
+        orden.style = " border: solid white 1px;"
         const ordenHeader = document.createElement("div");
 
         ordenHeader.className = "orden-Header d-flex justify-content-center"
@@ -142,122 +156,70 @@ function crearError(descripcion) {
 
 ///////// funciones de sistema
 
-function accesoALaWeb() {
-    document.querySelector("#nav").style.display = "flex";
+
+
+
+
+
+
+
+
+function inicioWeb() {
+    document.querySelector("#nav").style.display = "none";
+    document.querySelector("#orden").style.display = "none";
+    document.querySelector("#banner").style.display = "none";
+
+
+    let div = document.createElement("div");
+
+    let parrafoBienvenida = document.createElement("p");
+    parrafoBienvenida.innerText = "Bienvenido. Diganos su nombre por favor:";
+    div.appendChild(parrafoBienvenida);
+
+    let input = document.createElement("input");
+    input.className = "container-fluid mb-2 border border-dark border-2 rounded"
+    input.id = "input-nombre";
+    div.appendChild(input);
+
+    let boton = document.createElement("button");
+    boton.className = "btn btn-primary ";
+    boton.innerText = "ACEPTAR";
+    boton.onclick = confirmarLogin;
+    div.appendChild(boton);
+
+    document.querySelector("#contenedor").appendChild(div);
+}
+
+function confirmarLogin() {
+    let nombre = document.querySelector("#input-nombre").value;
+
+    if (chequearSiSonLetras(nombre) === false) {
+        crearError("Usted no ha ingresado un nombre.");
+    }
+    else {
+        pedirEdad(nombre);
+    }
+}
+function pedirEdad(nombre) {
     document.querySelector("#contenedor").replaceChildren();
 
-    let div1 = document.createElement("div");
-    div1.innerHTML = "<H2><u> Este es nuestro listado de Boxers Nacionales</u></H2>";
-    div1.className = "d-flex justify-content-center"
-    document.querySelector("#contenedor").appendChild(div1)
+    let div = document.createElement("div");
+    let parrafoEdad = document.createElement("p");
+    parrafoEdad.innerText = `Hola, ${nombre}. Indicanos tu edad`;
+    div.appendChild(parrafoEdad);
 
-    let div2 = document.createElement("div");
-    div2.className = "mt-4 col-12 row d-flex justify-content-evenly "
+    let input = document.createElement("input");
+    input.className = "container-fluid border border-dark mb-2 border-3 rounded"
+    input.id = "input-edad";
+    div.appendChild(input);
 
-    sistema(div2);
-
-    document.querySelector("#contenedor").appendChild(div2);
-
-
-
-
-
-
-}
-
-function chequeoUserYPass() {
-    if (textoNullOVacio(document.querySelector("#input-checkUser").value) || textoNullOVacio(document.querySelector("#input-checkPass").value)) {
-        alert("Usuario y/o contraseña erroneos. Intente nuevamente.")
-    } else {
-        accesoALaWeb();
-
-    }
-}
-
-function corroborarUser() {
-
-    if (textoNullOVacio(document.querySelector("#input-newUser").value) || textoNullOVacio(document.querySelector("#input-newPass").value)) {
-        crearError("Datos invalidos. Intente nuevamente");
-    } else {
-        document.querySelector("#contenedor").replaceChildren();
-
-        let divCheckUser = document.createElement("div");
-        let parrafoCheckUser = document.createElement("p");
-        parrafoCheckUser.innerText = "Reingresa tu nombre de usuario:";
-        divCheckUser.appendChild(parrafoCheckUser);
-
-        let inputUser = document.createElement("input");
-        inputUser.className = "border border-white border-2 rounded";
-        inputUser.id = "input-checkUser";
-        divCheckUser.appendChild(inputUser);
-
-        let divCheckPass = document.createElement("div");
-        let parrafoCheckPass = document.createElement("p");
-        parrafoCheckPass.innerText = "Reingresa tu contraseña:";
-        divCheckPass.appendChild(parrafoCheckPass);
-
-        let inputPass = document.createElement("input");
-        inputPass.className = "border border-white border-2 rounded";
-        inputPass.id = "input-checkPass";
-        divCheckPass.appendChild(inputPass);
-
-        let divBoton = document.createElement("div");
-        let boton = document.createElement("button");
-        boton.className = "btn btn-primary mt-2";
-        boton.innerText = "ACEPTAR";
-        boton.onclick = chequeoUserYPass;
-
-        divBoton.appendChild(boton);
-
-        document.querySelector("#contenedor").appendChild(divCheckUser);
-
-        document.querySelector("#contenedor").appendChild(divCheckPass);
-
-        document.querySelector("#contenedor").appendChild(divBoton);
-
-
-    }
-
-}
-
-
-function userNuevo() {
-    let divUser = document.createElement("div");
-    let parrafoNewUser = document.createElement("p");
-
-    parrafoNewUser.innerText = "Ingresa tu nuevo nombre de usuario por favor:";
-    divUser.appendChild(parrafoNewUser);
-
-    let inputUser = document.createElement("input");
-    inputUser.className = "border border-white border-2 rounded";
-    inputUser.id = "input-newUser";
-    divUser.appendChild(inputUser);
-
-
-    let divPass = document.createElement("div");
-    let parrafoNewPass = document.createElement("p");
-    parrafoNewPass.innerText = "Ingresa tu nueva contraseña por favor:";
-    divPass.appendChild(parrafoNewPass);
-
-    let inputPass = document.createElement("input");
-    inputPass.className = "border border-white border-2 rounded";
-    inputPass.id = "input-newPass";
-    divPass.appendChild(inputPass);
-
-
-    let divBoton = document.createElement("div");
     let boton = document.createElement("button");
-    boton.className = "btn btn-primary mt-2";
+    boton.className = "btn btn-primary";
     boton.innerText = "ACEPTAR";
-    boton.onclick = corroborarUser;
+    boton.onclick = validarEdad;
+    div.appendChild(boton);
 
-    divBoton.appendChild(boton);
-
-    document.querySelector("#user-nuevo").appendChild(divUser);
-
-    document.querySelector("#user-nuevo").appendChild(divPass);
-
-    document.querySelector("#user-nuevo").appendChild(divBoton);
+    document.querySelector("#contenedor").appendChild(div);
 
 }
 
@@ -291,65 +253,130 @@ function validarEdad() {
         crearError("Usted no ha ingresado un numero");
     }
 }
+function userNuevo() {
+    let divUser = document.createElement("div");
+    let parrafoNewUser = document.createElement("p");
 
-function pedirEdad(nombre) {
+    parrafoNewUser.innerText = "Ingresa tu nuevo nombre de usuario por favor:";
+    divUser.appendChild(parrafoNewUser);
+
+    let inputUser = document.createElement("input");
+    inputUser.className = "border border-dark border-2 rounded";
+    inputUser.id = "input-newUser";
+    divUser.appendChild(inputUser);
+
+
+    let divPass = document.createElement("div");
+    let parrafoNewPass = document.createElement("p");
+    parrafoNewPass.innerText = "Ingresa tu nueva contraseña por favor:";
+    divPass.appendChild(parrafoNewPass);
+
+    let inputPass = document.createElement("input");
+    inputPass.className = "border border-dark border-2 rounded";
+    inputPass.id = "input-newPass";
+    divPass.appendChild(inputPass);
+
+
+    let divBoton = document.createElement("div");
+    let boton = document.createElement("button");
+    boton.className = "btn btn-primary mt-2";
+    boton.innerText = "ACEPTAR";
+    boton.onclick = corroborarUser;
+
+    divBoton.appendChild(boton);
+
+    document.querySelector("#user-nuevo").appendChild(divUser);
+
+    document.querySelector("#user-nuevo").appendChild(divPass);
+
+    document.querySelector("#user-nuevo").appendChild(divBoton);
+
+}
+function corroborarUser() {
+
+    if (textoNullOVacio(document.querySelector("#input-newUser").value) || textoNullOVacio(document.querySelector("#input-newPass").value)) {
+        crearError("Datos invalidos. Intente nuevamente");
+    } else {
+        document.querySelector("#contenedor").replaceChildren();
+        let div = document.createElement("div")
+        let divH2 = document.createElement("h2")
+        divH2.innerText = "Reingresa tu usuario y contraseña"
+        div.appendChild(divH2)
+
+
+        let divCheckUser = document.createElement("div");
+        let parrafoCheckUser = document.createElement("p");
+        parrafoCheckUser.innerText = "Reingresa tu nombre de usuario:";
+        divCheckUser.appendChild(parrafoCheckUser);
+
+        let inputUser = document.createElement("input");
+        inputUser.className = "border border-dark border-2 rounded";
+        inputUser.id = "input-checkUser";
+        divCheckUser.appendChild(inputUser);
+
+        let divCheckPass = document.createElement("div");
+        let parrafoCheckPass = document.createElement("p");
+        parrafoCheckPass.innerText = "Reingresa tu contraseña:";
+        divCheckPass.appendChild(parrafoCheckPass);
+
+        let inputPass = document.createElement("input");
+        inputPass.className = "border border-dark border-2 rounded";
+        inputPass.id = "input-checkPass";
+        divCheckPass.appendChild(inputPass);
+
+        let divBoton = document.createElement("div");
+        let boton = document.createElement("button");
+        boton.className = "btn btn-primary mt-2";
+        boton.innerText = "ACEPTAR";
+        boton.onclick = chequeoUserYPass;
+
+        divBoton.appendChild(boton);
+
+        document.querySelector("#contenedor").appendChild(div)
+
+        document.querySelector("#contenedor").appendChild(divCheckUser);
+
+        document.querySelector("#contenedor").appendChild(divCheckPass);
+
+        document.querySelector("#contenedor").appendChild(divBoton);
+
+
+    }
+
+}
+function chequeoUserYPass() {
+    if (textoNullOVacio(document.querySelector("#input-checkUser").value) || textoNullOVacio(document.querySelector("#input-checkPass").value)) {
+        alert("Usuario y/o contraseña erroneos. Intente nuevamente.")
+    } else {
+        Swal.fire({
+            title: 'Has creado tu usuario satisfactoriamente ✔',
+            showClass: {
+                popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOutUp'
+            }
+        })
+        accesoALaWeb();
+
+    }
+}
+function accesoALaWeb() {
+    document.querySelector("#nav").style.display = "flex";
     document.querySelector("#contenedor").replaceChildren();
 
-    let div = document.createElement("div");
-    let parrafoEdad = document.createElement("p");
-    parrafoEdad.innerText = `Hola, ${nombre}. Indicanos tu edad`;
-    div.appendChild(parrafoEdad);
+    let div1 = document.createElement("div");
+    div1.innerHTML = "<H2><u> Este es nuestro listado de Boxers Nacionales</u></H2>";
+    div1.className = "d-flex justify-content-center"
+    document.querySelector("#contenedor").appendChild(div1)
 
-    let input = document.createElement("input");
-    input.className = "container-fluid border border-white mb-2 border-3 rounded"
-    input.id = "input-edad";
-    div.appendChild(input);
+    let div2 = document.createElement("div");
+    div2.className = "mt-4 col-12 row d-flex justify-content-evenly "
 
-    let boton = document.createElement("button");
-    boton.className = "btn btn-primary";
-    boton.innerText = "ACEPTAR";
-    boton.onclick = validarEdad;
-    div.appendChild(boton);
+    sistema(div2);
 
-    document.querySelector("#contenedor").appendChild(div);
+    document.querySelector("#contenedor").appendChild(div2);
 
 }
-
-
-function confirmarLogin() {
-    let nombre = document.querySelector("#input-nombre").value;
-
-    if (chequearSiSonLetras(nombre) === false) {
-        crearError("Usted no ha ingresado un nombre.");
-    }
-    else {
-        pedirEdad(nombre);
-    }
-}
-
-function inicioWeb() {
-    document.querySelector("#nav").style.display = "none";
-    document.querySelector("#orden").style.display = "none";
-
-
-    let div = document.createElement("div");
-
-    let parrafoBienvenida = document.createElement("p");
-    parrafoBienvenida.innerText = "Bienvenido. Diganos su nombre por favor:";
-    div.appendChild(parrafoBienvenida);
-
-    let input = document.createElement("input");
-    input.className = "container-fluid mb-2 border border-white border-2 rounded"
-    input.id = "input-nombre";
-    div.appendChild(input);
-
-    let boton = document.createElement("button");
-    boton.className = "btn btn-primary ";
-    boton.innerText = "ACEPTAR";
-    boton.onclick = confirmarLogin;
-    div.appendChild(boton);
-
-    document.querySelector("#contenedor").appendChild(div);
-}
-
-inicioWeb();
+// inicioWeb();
+accesoALaWeb();
